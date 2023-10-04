7e1d203cbb136d55bcfd7f940c44e0ab7a0c1469
whbs2002 <46540679+wacky9@users.noreply.github.com>
2023-10-04 19:17:11 -0400

    Delete OpenMat directory

--- OpenMat/eig.c
+++
@@ -1,5 +0,0 @@
-#include "lin_alg_mac.h"
-/*Transform a square matrix into Hessenberg form*/
-Mat* hessenberg(Mat* mat){
-
-}
--- OpenMat/internal.h
+++
@@ -1,2 +0,0 @@
-#include "lin_alg_mac.h"
-Mat* sub_matrix(Mat* mat, short row_len, short col_len, short row_index, short col_index);
